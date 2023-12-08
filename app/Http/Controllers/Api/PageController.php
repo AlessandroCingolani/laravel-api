43921990989fab5d152b53570cbf9186610adcbe
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-12-08 14:15:53 +0100

    [ add ] assets img if img is true or false

--- app/Http/Controllers/Api/PageController.php
+++ app/Http/Controllers/Api/PageController.php
@@ -17,6 +17,18 @@ class PageController extends Controller
     public function getSlugProject($slug)
     {
         $project = Project::where('slug', $slug)->with('technologies', 'type')->first();
-        return response()->json($project);
+
+        if ($project) {
+            $success = true;
+            if (!$project->image) {
+                $project->image = asset('img/placeholder.webp');
+            } else {
+                $project->image = asset('storage/' . $project->image);
+            }
+        } else {
+            $success = false;
+        }
+
+        return response()->json(compact('project', 'success'));
     }
 }
