780ea9e61aa3894edc993648273e7b7bb73b652f
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-12-13 15:22:57 +0100

    [ add ] validator and make datas required ecc

--- app/Http/Controllers/Api/LeadController.php
+++ app/Http/Controllers/Api/LeadController.php
@@ -4,10 +4,33 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class LeadController extends Controller
 {
     public function store(Request $request)
     {
+        $data = $request->all();
+
+        $validator = Validator::make(
+            $data,
+            [
+                'name' => 'required|min:2|max:255',
+                'email' => 'required|min:2|max:255',
+                'message' => 'required|min:2'
+
+            ],
+            [
+                'name.required' => 'Name is required',
+                'name.min' => 'Name must be at least :min character',
+                'name.max' => 'Name cannot exceed :max character',
+                'email.required' => 'Email is required',
+                'email.min' => 'Email must be at least :min character',
+                'email.max' => 'Email cannot exceed :max character',
+                'message.required' => 'Message is required',
+                'message.min' => 'Message must be at least :min character'
+            ]
+        );
+        return response()->json($data);
     }
 }
