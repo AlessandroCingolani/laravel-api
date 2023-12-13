a0b2cb6d2b9185a9475986f1fac5a1dcc7846095
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-12-13 14:08:52 +0100

    [ init ] view message sended

---
+++ resources/views/mail/new_contact.blade.php
@@ -0,0 +1,8 @@
+<div>
+
+    <h2>You have a new message:</h2>
+    <p><strong>Name:</strong>{{ $lead->name }}</p>
+    <p><strong>Email:</strong>{{ $lead->email }}</p>
+    <p><strong>Message:</strong></p>
+    <p>{{ $lead->message }}</p>
+</div>
